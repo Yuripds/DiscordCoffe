df33fc4ce2cdbfa1b7a520cb5bd6c2e8279cb892
Yuripds <yurisantosypds@gmail.com>
2022-01-25 10:35:12 -0300

    add rota para chat

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import {Box,Button,Text,TextField,Image} from '@skynexui/components';
 import appConfig from '../config.json';
+import React from 'react';
+import {useRouter} from 'next/router'
 
 function Titulo(props){
     const Tag = props.tag || 'h1' ;
@@ -20,36 +22,6 @@ function Titulo(props){
 }
 
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
-
-
 // function HomePage() {
 //     return (
 //         <div>
@@ -65,11 +37,11 @@ function GlobalStyle() {
 
 
  export default function PaginaInicial() {
-    const username = 'Yuripds';
-  
+    //const username = 'Yuripds';
+    const [username,setUsername] = React.useState('Yuripds');
+    const roteamento = useRouter();
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -96,6 +68,12 @@ function GlobalStyle() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infoDoEvento){
+                infoDoEvento.preventDefault();
+                roteamento.push('/chat');
+
+              }}
+
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -115,6 +93,11 @@ function GlobalStyle() {
                     mainColorHighlight: appConfig.theme.colors.primary[500],
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
+                }}
+                value = {username}
+                onChange={function handle(event){
+                  const valor = event.target.value;
+                  setUsername(valor);
                 }}
               />
               <Button
